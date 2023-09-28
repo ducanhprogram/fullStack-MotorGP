d258b86520c368aa973471d463001e31d65f700b
duc anh program <leducanhled@gmail.com>
2023-09-28 16:29:39 +0700

    Thêm Xóa người

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,6 +3,7 @@ const {
     getAllUsers,
     getUserById,
     updateUserById,
+    deleteUserById,
 } = require("../services/CRUDService");
 
 const getHomepage = async (req, res) => {
@@ -90,6 +91,19 @@ const postUpdateUser = async (req, res) => {
     res.redirect("/");
 };
 
+const postDeleteUser = async (req, res) => {
+    const userId = req.params.id;
+
+    let user = await getUserById(userId);
+    res.render("delete.ejs", { userEdit: user });
+};
+
+const postHandleRemoveUser = async (req, res) => {
+    const id = req.body.userId;
+
+    await deleteUserById(id);
+    res.redirect("/");
+};
 //Export ra nhiều biến
 module.exports = {
     getHomepage,
@@ -99,4 +113,6 @@ module.exports = {
     getCreatePage,
     getUpdatePage,
     postUpdateUser,
+    postDeleteUser,
+    postHandleRemoveUser,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -7,6 +7,8 @@ const {
     getCreatePage,
     getUpdatePage,
     postUpdateUser,
+    postDeleteUser,
+    postHandleRemoveUser,
 } = require("../controllers/homeController");
 const router = express.Router();
 
@@ -26,5 +28,9 @@ router.get("/update/:id", getUpdatePage);
 router.post("/create-user", postCreateUser);
 
 router.post("/update-user", postUpdateUser);
+
+router.post("/delete-user/:id", postDeleteUser);
+
+router.post("/delete-user", postHandleRemoveUser);
 
 module.exports = router;
